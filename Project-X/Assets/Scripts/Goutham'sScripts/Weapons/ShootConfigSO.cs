f217f361cdec72abf464c0272f32490d821d3661
GGgoutham <118740486+GGgoutham@users.noreply.github.com>
2023-12-25 19:18:08 +0530

    .

--- Project-X/Assets/Scripts/Goutham'sScripts/Weapons/ShootConfigSO.cs
+++ Project-X/Assets/Scripts/Goutham'sScripts/Weapons/ShootConfigSO.cs
@@ -17,33 +17,17 @@ public class ShootConfigSO : ScriptableObject, System.ICloneable
 
    // public ShootType ShootType = ShootType.FromGun;
 
-    /// <summary>
-    /// When <see cref="SpreadType"/> = <see cref="BulletSpreadType.Simple"/>, this value is used to compute the bullet spread.
-    /// This defines the <b>Maximum</b> offset in any direction that a single shot can offset the current forward of the gun. 
-    /// The range is from -x -> x, -y -> y, and -z -> z. 
-    /// </summary>
+
     [Header("Simple Spread")]
     public Vector3 spread = new Vector3(0.1f, 0.1f, 0.1f);
     [Header("Texture-Based Spread")]
-    /// <summary>
-    /// Multiplier applied to the vector from the center of <see cref="SpreadTexture"/> and the chosen pixel. 
-    /// Smaller values mean less spread is applied.
-    /// </summary>
+
     [Range(0.001f, 5f)]
     public float spreadMultiplier = 0.1f;
-    /// <summary>
-    /// Weighted random values based on the Greyscale value of each pixel, originating from the center of the texture is used to calculate the spread offset.
-    /// For more accurate guns, have strictly black pixels farther from the center of the image. 
-    /// For very inaccurate weapons, you may choose to define grey/white values very far from the center 
-    /// </summary>
+
     public Texture2D spreadTexture;
 
-    /**
-     * Calculates and returns the offset from "forward" that should be applied for the bullet
-     * based on <param name="ShootTime"/>. The closer to <see cref="MaxSpreadTime"/> this is, the
-     * larger area of <see cref="SpreadTexture"/> is read, or wider range of <see cref="Spread"/>
-     * is used, depending on <see cref="SpreadType"/>
-     */
+
     public Vector3 GetSpread(float ShootTime = 0)
     {
         Vector3 spread_ = Vector3.zero;
@@ -69,12 +53,7 @@ public class ShootConfigSO : ScriptableObject, System.ICloneable
         return spread_;
     }
 
-    /// <summary>
-    /// Reads provided <see cref="SpreadTexture"/> and uses a weighted random algorithm
-    /// to determine the spread. <param name= "ShootTime" /> indicates how long the player
-    /// has been shooting, larger values, closer to <see cref = "MaxSpreadTime" /> will sample
-    /// larger areas of the texture
-    /// </summary>         
+      
     private Vector2 GetTextureDirection(float ShootTime)
     {
         Vector2 halfSize = new Vector2(spreadTexture.width / 2f, spreadTexture.height / 2f);
